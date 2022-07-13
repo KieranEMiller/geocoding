b5081fe091847a7cc8c46ab479aefa91043112cc
kieranemiller <kieranemiller@gmail.com>
2022-07-13 18:07:03 -0400

    start to rework how web requests are issued so they can be tested; add interfaces that wrap web request and web response

---
+++ src/KEM.Maps.Geocoding/Services/BaseGeocodingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEM.Maps.Geocoding.Services
+{
+    public class BaseGeocodingService
+    {
+        public BaseGeocodingService()
+        {
+
+        }
+    }
+}
--- src/KEM.Maps.Geocoding/Services/Google/GoogleMaps.cs
+++ src/KEM.Maps.Geocoding/Services/Google/GoogleMaps.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -20,9 +21,16 @@ namespace KEM.Maps.Geocoding.Services
         public Coordinates GetLatLng(string address)
         {
             string requestUri = string.Format("https://maps.googleapis.com/maps/api/geocode/xml?key={1}&address={0}&sensor=false", Uri.EscapeDataString(address), API_KEY);
-
+            var client = new HttpClient();
+            client.PostAsync
             WebRequest request = WebRequest.Create(requestUri);
+            WebResponse response = request.GetResponse();
+            XDocument xdoc = XDocument.Load(response.GetResponseStream());
 
+            XElement result = xdoc.Element("GeocodeResponse").Element("result");
+            XElement locationElement = result.Element("geometry").Element("location");
+            XElement lat = locationElement.Element("lat");
+            XElement lng = locationElement.Element("lng");
 
             var coords = new LatLng(decimal.Parse(lat.Value), decimal.Parse(lng.Value));
 
