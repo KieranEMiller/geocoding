1c6c99f6acea3e43dd3f9507ba310e1cc9d28450
kieranemiller <kieranemiller@gmail.com>
2022-07-14 21:29:15 -0400

    update xml parser for google service to improve error handling and clean up implementation on parsing

--- src/KEM.Maps.Geocoding/Services/Google/GoogleResponseParser.cs
+++ src/KEM.Maps.Geocoding/Services/Google/GoogleResponseParser.cs
@@ -10,38 +10,38 @@ namespace KEM.Maps.Geocoding.Services.Google
 {
     public class GoogleResponseParser
     {
-        public Coordinates Parse(WebResponse response)
+        public Coordinate Parse(WebResponse response)
         {
             using (var stream = response.GetResponseStream())
             {
                 var xml = XDocument.Load(stream);
                 var xmlEleResponse =  xml.Element(GoogleConstants.Xml.ELEMENT_NAME_RESPONSE);
-                if(xmlEleResponse == null) 
-                    throw new Argument
+                if (xmlEleResponse == null)
+                    throw new ArgumentException($"response did not contain expected element {GoogleConstants.Xml.ELEMENT_NAME_RESPONSE}", GoogleConstants.Xml.ELEMENT_NAME_RESPONSE);
 
-
-                    .Element(GoogleConstants.Xml.ELEMENT_NAME_RESULT);
-
-                XElement locationElement = result
+                XElement locationElement = xmlEleResponse
+                    .Element(GoogleConstants.Xml.ELEMENT_NAME_RESULT)
                     .Element(GoogleConstants.Xml.ELEMENT_NAME_GEOMETRY)
                     .Element(GoogleConstants.Xml.ELEMENT_NAME_LOCATION);
 
-                XElement lat = locationElement.Element(GoogleConstants.Xml.ELEMENT_NAME_LATITUDE);
-                XElement lng = locationElement.Element(GoogleConstants.Xml.ELEMENT_NAME_LONGITUDE);
-
-                double lat = 
-
-                return new Geocoding.Coordinates()
+                return new Geocoding.Coordinate()
                 {
-                    Latitude=lat.Value,
-                    Longitude=lng
+                    Latitude=TryExtractCoordinateComponent(locationElement, GoogleConstants.Xml.ELEMENT_NAME_LATITUDE),
+                    Longitude= TryExtractCoordinateComponent(locationElement, GoogleConstants.Xml.ELEMENT_NAME_LONGITUDE)
                 };
             }
         }
 
-        private double ParseCoord(XElement ele)
+        private double TryExtractCoordinateComponent(XElement locationElement, string coordinateComponentName)
         {
+            XElement xmlElement = locationElement.Element(coordinateComponentName);
+            if (xmlElement == null)
+                throw new ArgumentException($"failed to locate location element {coordinateComponentName} from response");
 
+            double coordinateVal;
+            if (!double.TryParse(xmlElement.Value, out coordinateVal))
+                throw new ArgumentException($"failed to parse coordinate component {coordinateComponentName} from response", coordinateComponentName);
+            return coordinateVal;
         }
     }
 }
