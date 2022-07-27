97f74165e6fa880dd128566abbe96bd1e3013e3d
kieranemiller <kieranemiller@gmail.com>
2022-07-26 20:19:41 -0400

    add initial framework for calculating proximity between a collection of coordinates (IGeolocatableByCoordinates)

---
+++ src/KEM.Maps.Geocoding/Proximity/GeocodeProximity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEM.Maps.Geocoding.Proximity
+{
+    public class GeocodeProximity
+    {
+        public IList<ProximityResult> CalculateProximityMatrix(IList<IGeolocatableByCoordinates> inputCoords)
+        {
+
+        }
+    }
+}
---
+++ src/KEM.Maps.Geocoding/Proximity/ProximityResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEM.Maps.Geocoding.Proximity
+{
+    public class ProximityResult
+    {
+        public double DistanceInMeters { get; set; }
+
+        public IGeolocatableByCoordinates From { get; set; }
+
+        public IGeolocatableByCoordinates To { get; set; }
+    }
+}
