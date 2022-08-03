27e6ee9acc347f3d8e2a4eccf0f6d50153a9925c
kieranemiller <kieranemiller@gmail.com>
2022-08-02 22:39:21 -0400

    implement distance calculation using System.Device.Location.GeoCoordinate

--- src/KEM.Maps.Geocoding/Proximity/GeocodeProximity.cs
+++ src/KEM.Maps.Geocoding/Proximity/GeocodeProximity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Device.Location;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,34 @@ namespace KEM.Maps.Geocoding.Proximity
     {
         public IList<ProximityResult> CalculateProximityMatrix(IList<IGeolocatableByCoordinates> inputCoords)
         {
+            var results = new List<ProximityResult>();
 
+            foreach(var fromCoord in inputCoords)
+            {
+                foreach(var toCoord in inputCoords)
+                {
+                    //TODO: avoid computing distance to itself
+                    double distanceInMeters = CalculateDistanceInMeters(fromCoord, toCoord);
+                    var proximityResult = new ProximityResult()
+                    {
+                        From = fromCoord,
+                        To = toCoord,
+                        DistanceInMeters = distanceInMeters
+                    };
+                    results.Add(proximityResult);
+                }
+            }
+
+            return results;
+        }
+
+        public double CalculateDistanceInMeters(IGeolocatableByCoordinates from, IGeolocatableByCoordinates to)
+        {
+            var fromCoord = new GeoCoordinate(from.Latitude, from.Longitude);
+            var toCoord = new GeoCoordinate(to.Latitude, to.Longitude);
+
+            double distanceinMeters = fromCoord.GetDistanceTo(toCoord);
+            return distanceInMeters;
         }
     }
 }
