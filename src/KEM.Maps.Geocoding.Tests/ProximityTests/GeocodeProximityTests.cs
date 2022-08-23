3cf2cc3eed5e063b8c78620ae90ecc6c8d0ca109
kieranemiller <kieranemiller@gmail.com>
2022-08-23 19:04:32 -0400

    add initial test class for proximity calculations

---
+++ src/KEM.Maps.Geocoding.Tests/ProximityTests/GeocodeProximityTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEM.Maps.Geocoding.Tests.ProximityTests
+{
+    [TestFixture]
+    internal class GeocodeProximityTests
+    {
+        [Test]
+        public void tmp()
+        {
+
+        }
+    }
+}
