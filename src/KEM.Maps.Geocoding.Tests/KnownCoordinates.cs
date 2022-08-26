ed2bb2dffd592f16920d654cb87dece966657243
kieranemiller <kieranemiller@gmail.com>
2022-08-25 20:05:02 -0400

    add 3 known coordinates with latitude/longitude for locations on the east coast for use in tests

---
+++ src/KEM.Maps.Geocoding.Tests/KnownCoordinates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEM.Maps.Geocoding.Tests
+{
+    /// <summary>
+    /// sets of geocoordinates that are fixed and known for testing
+    /// </summary>
+    internal class KnownCoordinates
+    {
+        public static Coordinate NEWARK_PENN_STATION_NJ_USA = new Coordinate(40.7433514, -74.1793915);
+        public static Coordinate EMPIRE_STATE_BUILDING_NYC_USA = new Coordinate(40.7493788, -74.0196205);
+        public static Coordinate DEWEY_CHEATUM_AND_HOWE_BOSTON_MA_USA = new Coordinate(42.3733749, -71.1205517);
+    }
+}
