94244b176e05d285ea17f601dae928ad2956fb78
kieranemiller <kieranemiller@gmail.com>
2022-08-25 20:04:36 -0400

    add ctors for the coordinate class to initialize latitude and longitude

--- src/KEM.Maps.Geocoding/Coordinate.cs
+++ src/KEM.Maps.Geocoding/Coordinate.cs
@@ -9,6 +9,15 @@ namespace KEM.Maps.Geocoding
 {
     public class Coordinate : IGeocodingServiceResult
     {
+        public Coordinate()
+        { }
+
+        public Coordinate(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
         public double Latitude { get; set; }
 
         public double Longitude { get; set; }
